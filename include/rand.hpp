617cef8a7054c827fe6980c2a4bc26303e05221c
Amin Gholizad <amin.gholizad@gmail.com>
2025-11-14 02:36:14 +0100

    update to newer c++ standards. beter readablity and following core guidelines

--- include/rand.hpp
+++ include/rand.hpp
@@ -1,52 +1,56 @@
 #ifndef RAND_HPP
 #define RAND_HPP
 #pragma once
-#include <random>
-#include <algorithm>
 #include <chrono>
-namespace rnd
-{
-inline std::mt19937 &Generator()
-{
-    static std::random_device rd;
-    static unsigned int seed {(rd.entropy() == 0) ? static_cast<unsigned int>(std::chrono::system_clock::now().time_since_epoch().count()) : rd()};
-    static std::mt19937 gen{seed};
-    return gen;
+#include <concepts>
+#include <iterator>
+#include <random>
+namespace rnd {
+[[nodiscard]] constexpr std::mt19937 &Generator() {
+  static std::random_device random_device;
+  static unsigned int seed{
+      (random_device.entropy() == 0)
+          ? static_cast<unsigned int>(
+                std::chrono::system_clock::now().time_since_epoch().count())
+          : random_device()};
+  static std::mt19937 generator{seed};
+  return generator;
 }
-template <typename T, typename RandomGenerator>
-inline constexpr T unifrnd(T a, T b, RandomGenerator &g)
-{
-    if constexpr (std::is_integral_v<T>)
-    {
-        std::uniform_int_distribution<T> dis(a, b);
-        return dis(g);
-    }
-    else
-    {
-        std::uniform_real_distribution<T> dis(a, b);
-        return dis(g);
-    }
-}
+
 template <typename T>
-inline constexpr T unifrnd(T a, T b)
-{
-    return unifrnd<T>(a, b, Generator());
+concept NumericType = std::integral<T> || std::floating_point<T>;
+
+template <typename G>
+concept RandomGenerator = std::uniform_random_bit_generator<G>;
+
+template <typename I>
+concept Iterator = std::input_iterator<I>;
+
+template <NumericType T, RandomGenerator G>
+[[nodiscard]] constexpr T unifrnd(T lower_limit, T upper_limit, G &generator) {
+  if constexpr (std::is_integral_v<T>) {
+    std::uniform_int_distribution<T> dis(lower_limit, upper_limit);
+    return dis(generator);
+  } else {
+    std::uniform_real_distribution<T> dis(lower_limit, upper_limit);
+    return dis(generator);
+  }
 }
-inline constexpr double rand()
-{
-    return unifrnd<double>(0., 1.);
+template <NumericType T>
+[[nodiscard]] constexpr T unifrnd(T lower_limit, T upper_limit) {
+  return unifrnd<T>(lower_limit, upper_limit, Generator());
 }
-template <typename Iter, typename RandomGenerator>
-inline Iter select_randomly(Iter start, Iter end, RandomGenerator &g)
-{
-    std::uniform_int_distribution<long long int> dis(0, std::distance(start, end) - 1);
-    std::advance(start, dis(g));
-    return start;
+[[nodiscard]] constexpr double rand() { return unifrnd<double>(0., 1.); }
+template <Iterator I, RandomGenerator G>
+[[nodiscard]] constexpr I select_randomly(I start, I end, G &generator) {
+  std::uniform_int_distribution<long long int> dis(
+      0, std::distance(start, end) - 1);
+  std::advance(start, dis(generator));
+  return start;
 }
-template <typename Iter>
-inline Iter select_randomly(Iter start, Iter end)
-{
-    return select_randomly(start, end, Generator());
+template <Iterator I>
+[[nodiscard]] constexpr I select_randomly(I start, I end) {
+  return select_randomly(start, end, Generator());
 }
 } // namespace rnd
 #endif // !RAND_HPP
